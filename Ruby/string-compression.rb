f650e1107dcd0596151f66b9ed8caf562e91bd39
Tony Narisi <tonynarisi@gmail.com>
2016-09-24 20:12:45 -0400

    Write ruby method to compress string

---
+++ Ruby/string-compression.rb
@@ -0,0 +1,37 @@
+# This algorithm is to compress a string, to give a count of equivalent consecutive
+# letters, such that "aabbcddeeeeaa" would equal "2a2b1c2d4e2a". If the compressed
+# string has a length equal to or greater than the original string, the original string
+# will be returned.
+
+# Write the string compression method
+
+def string_compression(str)
+  new_str = ""
+  counter = 1
+  current_letter = str[0]
+  str.split("")[1..-1].each do |letter|
+    if letter == current_letter
+      counter += 1
+    else
+      new_str += counter.to_s + current_letter
+      current_letter = letter
+      counter = 1
+    end
+  end
+  new_str += counter.to_s + current_letter
+  if new_str.length >= str.length
+    return str
+  else
+    return new_str
+  end
+end
+
+# Create a string and call string compression method on it
+
+str = "aaabbeeddccccdd"
+
+puts string_compression(str)
+
+str = "abcdef"
+
+puts string_compression(str)
